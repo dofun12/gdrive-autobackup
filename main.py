e7b483cb69200aad833259fa75ed0f3597d2b27d
kevim <k.v.n.such@gmail.com>
2021-04-15 02:07:05 -0300

    Cron parametrizavel

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from gdrive.gdrive_api import GDriveApi
 from model.gdrive_config_model import GDriveConfigModel
 from model.general_config_model import GeneralConfigModel
 from model.mysql_config_model import MysqlConfigModel
-from utils import dateutils
+import argparse
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
 
 def now():
     return datetime.now()
@@ -25,48 +26,54 @@ def now():
 def now_str():
     return now().strftime("%m/%d/%Y - %H:%M:%S")
 
- 
 
-def do_backup_async():
+def do_backup_async(gdrive_config, mysql_config, general_config):
     now = datetime.now().strftime("%m/%d/%Y - %H:%M:%S")
     print(f'{now} Backuping....')
-    gdrive_config = GDriveConfigModel(config)
-    mysql_config = MysqlConfigModel(config)
-    general_config = GeneralConfigModel(config)
 
     gdrive_api = GDriveApi(gdrive_config)
     list = gdrive_api.list_files()
     if len(list) > gdrive_config.MAX_BACKUPS:
         gdrive_api.remove_oldest_file()
 
-    gbackup = Backup(mysql_config,gdrive_config,general_config)
+    gbackup = Backup(mysql_config, gdrive_config, general_config)
     gbackup.do_drive_upload()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    isrunning = False
+
+    parser = argparse.ArgumentParser(description='Optional app description')
+    parser.add_argument('--profile', type=str,
+                        help='Set the profile of config.ini')
+    parser.add_argument('--cron', action='store_true',
+                        help='Run on cron')
+
+    args = parser.parse_args()
     now = datetime.now().strftime("%m/%d/%Y - %H:%M:%S")
     print(f'{now} Starting...')
     default_config = 'config.ini'
-    if len(sys.argv) > 1:
-        profile = sys.argv[1]
-        if profile.startswith('--profile='):
-            default_config = 'config_' + profile.split("=")[1] + '.ini'
+
+    if args.profile:
+        default_config = 'config_' + args.profile + '.ini'
+
     config = configparser.ConfigParser()
     config.read(default_config)
 
-    if not isrunning:
-        do_backup_async()
+    iscron = args.cron
 
-    while(isrunning):
-        if pycron.is_now('*/2 * * * *'):
+    gdrive_config = GDriveConfigModel(config)
+    mysql_config = MysqlConfigModel(config)
+    general_config = GeneralConfigModel(config)
+
+    if not iscron:
+        do_backup_async(gdrive_config, mysql_config, general_config)
+        exit(0)
+    CRON = general_config.CRON
+    print(f'Starting cron... waiting for ( {CRON} )')
+    while iscron:
+        if pycron.is_now(CRON):
             now = datetime.now().strftime("%m/%d/%Y - %H:%M:%S")
             print(f'{now} Cronando....')
-            threading.Thread(target=do_backup_async, args=(), kwargs={}).start()
+            threading.Thread(target=do_backup_async, args=(gdrive_config, mysql_config, general_config), kwargs={}).start()
         time.sleep(60)
-
-
-
-
-
--- model/general_config_model.py
+++ model/general_config_model.py
@@ -1,9 +1,12 @@
 class GeneralConfigModel:
+
     MAX_SQL_DUMPS: int
     MAX_COMPRESSED_DUMPS: int
     KEEP_LOCAL_BACKUPS: bool
+    CRON: str
 
     def __init__(self, config):
+        self.CRON = config['GENERAL']['CRON']
         self.MAX_SQL_DUMPS = int(config['GENERAL']['MAX_SQL_DUMPS'])
         self.MAX_COMPRESSED_DUMPS = int(config['GENERAL']['MAX_COMPRESSED_DUMPS'])
         self.KEEP_LOCAL_BACKUPS = 'true' == config['GENERAL']['KEEP_LOCAL_BACKUPS']
